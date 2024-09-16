f5fdb2409b64b5f903621f7138c5877b9ceea6e6
Nicolascv31 <gonzalocoaguilavaldivia@gmail.com>
2024-09-15 21:32:12 -0500

    feat: readFile

--- src/main/java/com/ucsp/app/CsApplication.java
+++ src/main/java/com/ucsp/app/CsApplication.java
@@ -2,5 +2,8 @@ package com.ucsp.app;
 
 public class CsApplication {
     public static void main(String[] args) {
+        String file = "D:/compiladores/Scanner/prueba.txt";
+        readFile lector = new readFile(file);
+        lector.readFile();
     }
 }

6b7dfbea4075600177c97a6107b25974bfa293a5
wChris7s <cw.aranibar.solaligue@gmail.com>
2024-09-19 16:54:02 -0500

    refac: delete unused classes

--- src/main/java/com/ucsp/app/domain/validators/TokenValidator.java
+++
@@ -1,9 +0,0 @@
-package com.ucsp.app.domain.validators;
-
-public interface TokenValidator {
-  boolean isKeyword(String token);
-
-  boolean isOperator(String token);
-
-  boolean isDelimiter(String token);
-}
